3c2b48c93b0a9811bf526df876f3bd8409e81aa8
gunner73 <amit.gunner73@gmail.com>
2020-11-12 14:36:31 +0600

    Codeforces (Problem ID: 615A - Bulbs)

---
+++ Bulbs.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+#define ll long long
+#define tc(t) while(t--)
+#define endl "\n"
+#define IOS ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+
+
+int main()
+{
+    IOS;
+
+    ll t,bulb,x,numberOfBulb ;
+
+    cin >> t >> bulb ;
+
+    ll arr[bulb];
+    memset(arr,0,sizeof(arr));
+
+    tc(t)
+    {
+        cin >> x ;
+        for(int i=0;i<x;i++)
+        {
+            cin >> numberOfBulb;
+            arr[numberOfBulb-1] = 1;
+        }
+    }
+
+    /*for(int i=0;i<bulb;i++)
+    {
+        cout << arr[i] ;
+    }*/
+
+
+    bool temp = true ;
+    for(int i=0;i<bulb;i++)
+    {
+        if(arr[i] == 0)
+        {
+            cout << "NO" ;
+            temp = false ;
+            break;
+        }
+    }
+    if(temp == true)
+    {
+        cout << "YES" ;
+    }
+
+
+    return 0 ;
+}
