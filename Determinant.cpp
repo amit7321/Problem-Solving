87b9eb06d818c685162359940c835258c2c98122
gunner73 <amit.gunner73@gmail.com>
2020-11-27 23:30:56 +0600

    AtCoder Beginner Contest 184 (Problem - A)

---
+++ Determinant.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+#define ll long long
+#define tc(t) while(t--)
+#define endl "\n"
+#define IOS ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+
+
+int main()
+{
+    IOS ;
+    ll a,b,c,d ;
+
+    cin >> a >> b ;
+    cin >> c >> d ;
+
+    cout << (a*d)-(b*c) ;
+
+
+    return 0 ;
+}
